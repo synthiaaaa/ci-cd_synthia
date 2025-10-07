56afe4fcbd4f0930c46f2d70fc6b6a5e40174702
synthiaaaa <synthia.kabango93@gmail.com>
2025-10-07 15:24:58 +0200

    correction

--- app/main.py
+++ app/main.py
@@ -2,5 +2,8 @@ def add(a: int, b: int) -> int:
     """Addition simple."""
     return a + b
 
-if __name__ == "__main__":
+
+if __name__ == "__main__": 
     print("Résultat :", add(2, 3))
+
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,5 @@
 from app.main import add
 
-def test_add():
+
+def test_add():  # 👈 ajoute une ligne vide avant la fonction
     assert add(2, 3) == 5
