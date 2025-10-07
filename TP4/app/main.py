bef34388ac79a72d7517a9e7a4386506722d778c
synthiaaaa <synthia.kabango93@gmail.com>
2025-10-07 16:03:58 +0200

    update

---
+++ TP4/app/main.py
@@ -0,0 +1,8 @@
+def add(a: int, b: int) -> int:
+    """Addition simple."""
+    return a + b
+
+
+# NE PAS exécuter de code au niveau global
+if __name__ == "__main__":
+    print("Résultat :", add(2, 3))
