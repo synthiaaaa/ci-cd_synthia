46fbc18a88482a13728bf246c9df46e7dfb52bcc
synthiaaaa <synthia.kabango93@gmail.com>
2025-10-08 11:03:05 +0200

    new build1

--- TP4/app/main.py
+++ TP4/app/main.py
@@ -1,15 +1,10 @@
 def count_words(words):
-    """Retourne le nombre de mots dans la liste."""
     return len(words)
 
 
 def longest_word(words):
-    """Retourne le mot le plus long de la liste ou une chaîne vide si la liste est vide."""
-    if not words:
-        return ""
-    return max(words, key=len)
+    return max(words, key=len) if words else ""
 
 
-def contains_word(words, word):
-    """Vérifie si 'word' est présent dans la liste 'words'."""
-    return word in words
+def contains_word(words, target):
+    return target in words
